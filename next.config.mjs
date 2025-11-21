9b7b976d9f97c221112dbe3ae553d19eb210eb35
SONG-HJC <wjdcjf0304@naver.com>
2025-11-21 20:43:49 +0900

    초기 설정

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,17 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+
+  async rewrites() {
+    return [
+      {
+        // 프론트엔드에서 '/api/...'로 요청을 보내면
+        source: '/api/:path*',
+        // 실제로는 이 백엔드 주소로 보낸 것처럼 처리해라
+        destination: 'https://sorikyul.onrender.com/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -23,7 +23,7 @@ export default function LoginPage() {
     setErrorMsg(""); 
 
     try {
-      const response = await fetch(`${API_BASE_URL}/api/auth/login`, {
+      const response = await fetch(`/api/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
