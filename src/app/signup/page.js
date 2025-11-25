77cfe1a9eeb211d7beca63265400ecd6fea485d7
SONG-HJC <wjdcjf0304@naver.com>
2025-11-25 12:14:32 +0900

    회원가입 페이지

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -62,7 +62,7 @@ export default function SignUpPage() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-8 bg-gray-100">
+    <div className="flex flex-col items-center justify-center min-h-screen p-8">
       <div className="w-full max-w-sm p-10 bg-white rounded-2xl shadow-xl text-center">
         <h1 className="text-xl font-bold text-gray-800 mb-5">
           소리 센서 모니터링 서비스
